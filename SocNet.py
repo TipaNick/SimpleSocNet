20c7deef9c84ba48573ee0ce5b12686f84ea5f6b
Egor Chasovskih <visualjaw3@gmail.com>
2024-03-30 15:29:15 +0700

    Added login() trivial realisation

--- SocNet.py
+++ SocNet.py
@@ -2,3 +2,5 @@ class SocNet:
 	def __init__(self):
 		pass
 		
+	def login(self, login, password):
+		return 1
--- TestSocNet.py
+++ TestSocNet.py
@@ -4,3 +4,8 @@ from SocNet import SocNet
 def test_class_socnet_create():
 	socnet = SocNet()
 	assert socnet is not None
+
+def test_login1():
+	# todo: finish method
+	socnet = SocNet()
+	assert socnet.login("admin", "password") == 1
