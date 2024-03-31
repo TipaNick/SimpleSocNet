03ae973ad71d65dc65312cf1c72b93e5c131a831
Egor Chasovskih <visualjaw3@gmail.com>
2024-03-31 17:45:26 +0700

    Added send_message(), add_friend(), delete_friend() trivial realisation

--- SocNet.py
+++ SocNet.py
@@ -1,6 +1,16 @@
 class SocNet:
+	messages = []
 	def __init__(self):
 		pass
 		
 	def login(self, login, password):
 		return 1
+
+	def send_message(self, text, id_friend):
+		return "success"
+
+	def add_friend(self, name_friend):
+		return "success"
+
+	def delete_friendd(self, num_friend):
+		return "success"
--- TestSocNet.py
+++ TestSocNet.py
@@ -5,7 +5,22 @@ def test_class_socnet_create():
 	socnet = SocNet()
 	assert socnet is not None
 
-def test_login1():
+def test_login_trivial():
 	# todo: finish method
 	socnet = SocNet()
 	assert socnet.login("admin", "password") == 1
+
+def test_send_message():
+	# todo: finish method
+	socnet = SocNet()
+	assert socnet.send_message("test", 1) == "success"
+
+def test_add_friend():
+	# todo: finish method
+	socnet = SocNet()
+	assert socnet.add_friend("test") == "success"
+
+def test_delete_friend():
+	# todo: finish method
+	socnet = SocNet()
+	assert socnet.delete_friendd(1) == "success"
