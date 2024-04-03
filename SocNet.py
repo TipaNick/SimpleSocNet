cb84573c7b33e28a7d819d7cc99bed54eb5fb21f
Egor Chasovskih <visualjaw3@gmail.com>
2024-04-03 12:31:13 +0700

    Tests fix and refactoring
    Refactored: test_send_message(), test_add_friend_not_created(),
    test_add_friend_already_created(),
    test_delete_friend(),
    test_get_list_friends(),
    Finish send_message() method

--- SocNet.py
+++ SocNet.py
@@ -1,8 +1,11 @@
 class SocNet:
-	ID_LAST_USER = 1
-	MESSAGES = []
-	USERS = [[1, "admin", "password"]]
-	FRIENDS = [[1, []]]
+	__ID_LAST_USER = 10
+	__MESSAGES = [[1, 2, "Test text"],
+				[2, 1, "Test text 2"]]
+	__USERS = [[1, "admin", "password"],
+			[2, "test", "test"],
+			[3, "friend1", "friend1"]]
+	__FRIENDS = [[1, [2, 3]], [2, [1]], [3,[]]]
 
 	def __init__(self):
 		pass
@@ -11,37 +14,73 @@ class SocNet:
 		# Метод авторизации с помощью логина и пароля
 		# param login - любая строка
 		# param password - любая строка
-		# return - id пользателя
-		for user in self.USERS:
+		# return - id пользователя
+		for user in self.__USERS:
 			if user[1] == login and user[2] == password:
 				return user[0]
 		return -1
 
-	def send_message(self, text, id_friend):
-		# todo: finish method
+	def send_message(self, id_user, id_friend, text):
+		# Метод, добавляющий сообщение
+		# param id_user - любое число
+		# param id_friend - любое число
+		# param text - любая строка
+		# return - результат операции 
+		self.__MESSAGES.append([id_user, id_friend, text])
 		return "success"
 
-	def add_friend(self, name_friend):
+	def add_friend(self, id_user, name_friend):
 		# todo: finish method
-		return "success"
+		friend_id = self.find_user_by_name(name_friend)
+		if friend_id == -1:
+			return "User is not existed"
+		for friend in self.__FRIENDS:
+			if friend[0] == id_user:
+				if friend_id in friend[1]:
+					return "User already in friend list"
+				else:
+					friend[1].append(friend_id)
+					return "success"
 
-	def delete_friend(self, num_friend):
+	def delete_friend(self, id_user, num_friend):
 		# todo: finish method
 		return "success"
 
 	def find_user_by_name(self, name):
-		# todo: finish method
-		return 1
+		# Метод, возвращающий id по имени
+		# param name - любая строка
+		# return - id пользователя
+		for user in self.__USERS:
+			if user[1] == name:
+				return user[0]
+		return -1
 
 	def create_user(self, login, password):
 		# Метод, создающий пользователя
 		# param login - любая строка
 		# param password - любая строка
 		# return - результат операции
-		for user in self.USERS:
+		for user in self.__USERS:
 			if user[1] == login:
 				return -1
-		self.ID_LAST_USER += 1
-		self.USERS.append([self.ID_LAST_USER, login, password])
-		self.FRIENDS.append([self.ID_LAST_USER, []])
-		return 2
+		self.__ID_LAST_USER += 1
+		self.__USERS.append([self.__ID_LAST_USER, login, password])
+		self.__FRIENDS.append([self.__ID_LAST_USER, []])
+		return self.__ID_LAST_USER
+
+	def get_list_friends(self, id_user):
+		# Метод, выводящий список друзей
+		# param id_user - любое число
+		# return - массив друзей
+		list_id_friends = None
+		friends_list = []
+		for friends in self.__FRIENDS:
+			if friends[0] == id_user:
+				list_id_friends = friends[1]
+				break
+		for friend in list_id_friends:
+			for user in self.__USERS:
+				if friend == user[0]:
+					friends_list.append([user[0], user[1]])
+					break
+		return friends_list
--- TestSocNet.py
+++ TestSocNet.py
@@ -13,26 +13,34 @@ def test_login_invalid_user():
 	socnet = SocNet()
 	assert socnet.login("wrong_user", "wrong_user") == -1
 
-def test_send_message_1():
+def test_send_message():
 	socnet = SocNet()
-	assert socnet.send_message("test", 1) == "success"
+	assert socnet.send_message(1, 2, "test") == "success"
 
-def test_add_friend_1():
+def test_add_friend_not_created():
 	socnet = SocNet()
-	assert socnet.add_friend("test") == "success"
+	assert socnet.add_friend(2, "friend1") == "success"
 
-def test_delete_friend_1():
+def test_add_friend_already_created():
 	socnet = SocNet()
-	assert socnet.delete_friend(1) == "success"
+	assert socnet.add_friend(1, "test") == "User already in friend list"
 
-def test_find_user_by_name_1():
+def test_delete_friend():
+	socnet = SocNet()
+	assert socnet.delete_friend(1, 2) == "success"
+
+def test_find_user_by_name():
 	socnet = SocNet()
 	assert socnet.find_user_by_name("admin") == 1
 
-def test_create_user_not_existed():
+def test_create_user_not_created():
 	socnet = SocNet()
-	assert socnet.create_user("test1", "test1") == 2
+	assert socnet.create_user("test1", "test1") == 11
 
-def test_create_user_already_existed():
+def test_create_user_already_created():
 	socnet = SocNet()
 	assert socnet.create_user("admin", "password") == -1
+
+def test_get_list_friends():
+	socnet = SocNet()
+	assert socnet.get_list_friends(1) == [[2, "test"], [3, "friend1"]]
